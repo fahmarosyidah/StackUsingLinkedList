96468e877eecdda67b55c8e60ccec85eeba49516
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-11-30 15:39:17 +0700

    Add project files.

---
+++ StackUsingLinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackUsingLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
