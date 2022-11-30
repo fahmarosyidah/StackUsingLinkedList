2b7f7867710f3ace1a0e6502560bd7e4e2bd6471
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-11-30 16:07:51 +0700

    Membuat objek baru untuk class Stack

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -66,7 +66,7 @@ namespace StackUsingLinkedList
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stacks s = new Stacks();
         }
     }
 }
