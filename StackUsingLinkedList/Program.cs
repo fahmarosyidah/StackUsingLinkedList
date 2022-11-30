eebb9fdb9cad1e03f6b5704884a1b7fcd0683548
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-11-30 16:15:45 +0700

    Menggunakan switch case untuk menampilkan menu yang dipilih

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -78,7 +78,30 @@ namespace StackUsingLinkedList
                 Console.Write("\nEnter your choice: ");
                 string sInput = Console.ReadLine();
                 char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
-
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+                    case '3':
+                        s.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+                        break;
+                }
             }
         }
     }
